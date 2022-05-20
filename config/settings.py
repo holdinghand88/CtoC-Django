0dfe54120c95e55bb91bec5f1e717d0f2b64cd33
topcode111 <aldahir.martinez9197@gmail.com>
2022-05-21 01:55:15 +0900

    fixing

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend'
 )
-SITE_ID = 1
+SITE_ID = 2
 LOGIN_REDIRECT_URL = '/'
 
 # CRISPY FORMS
